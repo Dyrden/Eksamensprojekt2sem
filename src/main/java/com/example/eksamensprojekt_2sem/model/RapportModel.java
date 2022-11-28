9edb9ef6ea02e50d2deada386e878b93540fcbb5
bjoexxxx <bjoe6702@stud.kea.dk>
2022-11-28 10:59:18 +0100

    Updated all models

--- src/main/java/com/example/eksamensprojekt_2sem/model/RapportModel.java
+++ src/main/java/com/example/eksamensprojekt_2sem/model/RapportModel.java
@@ -1,4 +1,36 @@
 package com.example.eksamensprojekt_2sem.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class RapportModel {
+    private LocalDateTime dato;
+    private List<SkadeModel> skader;
+
+    public RapportModel(LocalDateTime dato, List<SkadeModel> skader){
+        this.dato = dato;
+        this.skader = skader;
+    }
+
+    public LocalDateTime getDato() {
+        return dato;
+    }
+
+    public void setDato(LocalDateTime dato) {
+        this.dato = dato;
+    }
+
+    public List<SkadeModel> getSkader() {
+        return skader;
+    }
+
+    public void setSkader(List<SkadeModel> skader) {
+        this.skader = skader;
+    }
+
+    public String toString(){
+        return "Dato: "+dato+ "\n" +
+                "Skader: "+skader;
+
+    }
 }
--- src/main/java/com/example/eksamensprojekt_2sem/model/SkadeModel.java
+++ src/main/java/com/example/eksamensprojekt_2sem/model/SkadeModel.java
@@ -1,4 +1,33 @@
 package com.example.eksamensprojekt_2sem.model;
 
 public class SkadeModel {
+    private String navn;
+    private double pris;
+
+    public SkadeModel(String navn, double pris){
+        this.navn = navn;
+        this.pris = pris;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
+
+    public String toString(){
+        return "Navn: "+navn+ "\n" +
+                "Pris: "+pris;
+
+    }
 }
