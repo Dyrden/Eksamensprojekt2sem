16b6110f74d005baa7664f976debde5afeba793c
ferh0162 <ferh0162@stud.kea.dk>
2022-11-29 11:24:40 +0100

    Tilføjet nye sider til kundebooking

--- src/main/java/com/example/eksamensprojekt_2sem/controller/DataRegController.java
+++ src/main/java/com/example/eksamensprojekt_2sem/controller/DataRegController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DataRegController {
 
-    @GetMapping("/dataRegistering")
+    @GetMapping("/dataRegistrering")
     public String visDataReg(){
-        return "html/dataRegistering";
+        return "html/dataRegistrering/dataRegistrering";
     }
 }
---
+++ src/main/java/com/example/eksamensprojekt_2sem/controller/Rapporter.java
@@ -0,0 +1,11 @@
+package com.example.eksamensprojekt_2sem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Rapporter {
+
+  @GetMapping("/seRapporter")
+  public String visForretningsUdviklere(){
+    return "html/seRapporter";
+  }
+}
