a74f2b434303e3aafa8a65876e38b77aa95aa515
bjoexxxx <bjoe6702@stud.kea.dk>
2022-11-29 13:42:37 +0100

    Der er nu en post metode til at tilgå tilføjskader fra skadeOgUdbedring

--- src/main/java/com/example/eksamensprojekt_2sem/controller/SkadeOgUdController.java
+++ src/main/java/com/example/eksamensprojekt_2sem/controller/SkadeOgUdController.java
@@ -2,6 +2,7 @@ package com.example.eksamensprojekt_2sem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SkadeOgUdController {
@@ -11,7 +12,7 @@ public class SkadeOgUdController {
         return "html/skadeOgUdbedring/skadeOgUdbedring";
     }
 
-    @GetMapping("/tilfoej")
+    @PostMapping("/tilfoej")
     public String tilfoejSkade(){
         return "html/skadeOgUdbedring/tilfoejSkade";
     }
