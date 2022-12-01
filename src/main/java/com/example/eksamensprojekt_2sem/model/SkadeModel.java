2c90430b559b605e3bce58826bbab4aa35d76780
ferh0162 <ferh0162@stud.kea.dk>
2022-12-01 12:17:28 +0100

    rettet SkadeModel

--- src/main/java/com/example/eksamensprojekt_2sem/model/SkadeModel.java
+++ src/main/java/com/example/eksamensprojekt_2sem/model/SkadeModel.java
@@ -1,17 +1,29 @@
 package com.example.eksamensprojekt_2sem.model;
 
 public class SkadeModel {
+    private int id;
     private String skadensPlacering;
     private String skadensBeskrivelse;
     private double skadensPris;
+    private int rapportID;
 
-    public SkadeModel(String skadensPlacering, String skadensBeskrivelse, double skadensPris) {
+    public SkadeModel(int id, String skadensPlacering, String skadensBeskrivelse, double skadensPris, int rapportID) {
+        this.id = id;
         this.skadensPlacering = skadensPlacering;
         this.skadensBeskrivelse = skadensBeskrivelse;
         this.skadensPris = skadensPris;
+        this.rapportID = rapportID;
     }
-    public SkadeModel(){
 
+    public SkadeModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getSkadensPlacering() {
@@ -38,11 +50,21 @@ public class SkadeModel {
         this.skadensPris = skadensPris;
     }
 
+    public int getRapportID() {
+        return rapportID;
+    }
+
+    public void setRapportID(int rapportID) {
+        this.rapportID = rapportID;
+    }
+
     @Override
     public String toString() {
-        return
+        return "SkadeModel{" +
+            "id=" + id +
             "skadensPlacering='" + skadensPlacering + '\'' +
-            ", skadensBeskrivelse='" + skadensBeskrivelse + '\'' +
-            ", skadensPris=" + skadensPris;
+            "skadensBeskrivelse='" + skadensBeskrivelse + '\'' +
+            "skadensPris=" + skadensPris +
+            "rapportID=" + rapportID;
     }
 }
