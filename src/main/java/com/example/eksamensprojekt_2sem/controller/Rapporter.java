908c78bbc75f4bfe5a4e508bee6fdc52f89cb2a6
ferh0162 <ferh0162@stud.kea.dk>
2022-12-06 16:16:03 +0100

    Se rapporter fixet

--- src/main/java/com/example/eksamensprojekt_2sem/controller/Rapporter.java
+++ src/main/java/com/example/eksamensprojekt_2sem/controller/Rapporter.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class Rapporter {
@@ -29,24 +30,34 @@ private SkadeRepository skadeRepository = new SkadeRepository();
     model.addAttribute("bil", bilRepository.visSpecifikBil(vognNummer));
     return "html/rapporter/seRapport";
   }
-  /*
-  @GetMapping("/seSkadeRapport/{vognNummer}")
+  @GetMapping("/vaelgRapportRapporter/{vognNummer}")
   //Ferhat er ansvarlig for denne metode
-  public String visSkader(@PathVariable("vognNummer") String vognNummer, Model model, HttpSession session){
+  public String vaelgRapport(@PathVariable("vognNummer") String vognNummer, Model model, HttpSession session){
     model.addAttribute("bil", bilRepository.visSpecifikBil(vognNummer));
 
     //Her henter vi den valgte bils rapport
     //Så kan vi efterfølgende bruge rapportens id til at modtage skaderne
-    RapportModel rapport = rapportRepository.hentRapportFraVognNummer(vognNummer);
-    model.addAttribute("rapport", rapportRepository.hentRapportFraVognNummer(vognNummer));
+    List<RapportModel> rapporter = rapportRepository.hentRapportFraVognNummer(vognNummer);
+    model.addAttribute("rapporter", rapportRepository.hentRapportFraVognNummer(vognNummer));
 
+
+    //Opretter en session og sætter bil til den bil vi er inde på i skader
+    session.setAttribute("bil", bilRepository.visSpecifikBil(vognNummer));
+
+    return "html/rapporter/vaelgRapport";
+  }
+
+  @GetMapping("/seSkadesRapport/{rapportID}")
+  //Ferhat er ansvarlig for denne metode
+  public String visSkader(@PathVariable("rapportID") String rapportID, Model model, HttpSession session){
     //Når vi nu har bilens rapport, så kan vi tilgå rapporten
     //vi henter alle skaderne fra rapportens id.
     //Rapportens id har vi fået fra tildigere kode gennem bilens vognNummer
-    model.addAttribute("skader",skadeRepository.skafSkaderFraRapport(rapport.getId()));
+    model.addAttribute("skader",skadeRepository.skafSkaderFraRapport(rapportID));
 
+    model.addAttribute("bil", session.getAttribute("bil"));
     return "html/rapporter/seSkadesRapport";
   }
 
-   */
+
 }
